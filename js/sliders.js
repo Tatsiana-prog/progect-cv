d1e1c19dafcc4cf6636133fd046da96eb11ce8b5
tatsiana-prog <programvtv07@gmail.com>
2024-04-28 22:33:28 +0300

    feat:changed code for carousel in skills

--- js/sliders.js
+++ js/sliders.js
@@ -1,22 +1,46 @@
-//подключение карусели begin в секции Skills
-const carouselLine = document.querySelector('.carousel-line1');
-const nextButton = document.querySelector('.carousel__arrow-next');
+//подключение карусели секции skills
+const cards = document.querySelector('.carousel-line1');
 const prevButton = document.querySelector('.carousel__arrow-prev');
-const cardWidth = 420;
+const nextButton = document.querySelector('.carousel__arrow-next');
+let position = 0;
 
-nextButton.addEventListener('click', () => {
-  carouselLine.scrollBy({
-    left: cardWidth,
-    behavior: 'smooth'
-  });
+const calculateCardWidth = () => {
+  const containerWidth = cards.offsetWidth;
+  const numCards = cards.children.length;
+  const gap = 80; // Gap between cards
+  return ((containerWidth - gap * (numCards - 1)) / numCards) + 'px';
+};
+
+const nextSlide = () => {
+  const maxPosition = (cards.children.length - 1) * (cards.children[0].offsetWidth + 80);
+  if (position < maxPosition) {
+    position += cards.children[0].offsetWidth + 80;
+  } else {
+    position = 0;
+  }
+  cards.style.left = -position + 'px';
+};
+
+const prevSlide = () => {
+  if (position > 0) {
+    position -= cards.children[0].offsetWidth + 80;
+  } else {
+    position = (cards.children.length - 1) * (cards.children[0].offsetWidth + 80);
+  }
+  cards.style.left = -position + 'px';
+};
+
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
+
+window.addEventListener('resize', () => {
+  position = 0;
+  cards.style.left = '0px';
+  cards.style.width = calculateCardWidth();
 });
 
-prevButton.addEventListener('click', () => {
-  carouselLine.scrollBy({
-    left: -cardWidth,
-    behavior: 'smooth'
-  });
-});
+cards.style.width = calculateCardWidth();
+
 //подключение карусели end
 
 
